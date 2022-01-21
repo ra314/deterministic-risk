e51b681df62de06f6f6ab6994b7e150c0b720018
ra314 <rahulgandharva20122000@gmail.com>
2022-01-21 23:02:27 +1100

    Pressing enter in IP screen goes to next menu.
    Closes #78.

--- Scripts/UI/IP Select.gd
+++ Scripts/UI/IP Select.gd
@@ -6,6 +6,8 @@ onready var _root: Main = get_tree().get_root().get_node("Main")
 func _ready():
 	var container = $CenterContainer/VBoxContainer
 	container.get_node("Connect/Button").connect("button_down", self, "_load_scene", ["UI/Waiting"])
+	$CenterContainer/VBoxContainer/HBoxContainer/TextEdit.connect("text_changed", self, "check_for_newline")
+	# Loading the previously used IP address
 	if _root.stored_IP:
 		$CenterContainer/VBoxContainer/HBoxContainer/TextEdit.text = _root.stored_IP
 	$TextureButton.connect("button_down", self, "back")
@@ -22,10 +24,16 @@ func back():
 	_root.scene_manager._replace_scene(scene)
 
 func _load_scene(scene_str):
-	# Slicing out the IP address
-	var server_IP = $CenterContainer/VBoxContainer/HBoxContainer/TextEdit.text
+	var server_IP = $CenterContainer/VBoxContainer/HBoxContainer/TextEdit.text.strip_edges(true, true)
 	_root.guest(server_IP)
 	_root.stored_IP = server_IP
 	
 	var scene = _root.scene_manager._load_scene(scene_str)
 	_root.scene_manager._replace_scene(scene)
+
+func check_for_newline():
+	var textedit = $CenterContainer/VBoxContainer/HBoxContainer/TextEdit
+	if not textedit:
+		return
+	if textedit.text[textedit.text.length()-1] == '\n':
+		$CenterContainer/VBoxContainer/Connect/Button.emit_signal("button_down")
